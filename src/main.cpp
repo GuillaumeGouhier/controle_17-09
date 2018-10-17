c7a0b6bb4de2d4f5a94508e061846eccae178815
Guillaume.Gouhier <guillaume.gouhier1999@gmail.com>
2018-10-17 11:50:48 +0200

    everything functionality works

--- src/main.cpp
+++ src/main.cpp
@@ -7,40 +7,61 @@
 using namespace std;
 
 
-
 int main()
 {
 	vector<int> num_list;
 	string tmp_input = "0";
-
+	int choice;
 	float tmp_num;
 	while(true){
-		cout<<"Quel est votre nombre?"<<endl;
-		cin>>tmp_num;
-		num_list.push_back(tmp_num);
-		sort(num_list.begin(), num_list.end());
+		
+		cout<<"Voici la liste en cours: "<<endl;
 		for(int i = 0; i < num_list.size(); i++){
-			cout<<num_list[i]<<endl;
+					cout<<num_list[i]<<endl;
+				}
+		cout<<"Que souhaitez vous faire? (1 pour ajouter un nombre, 2 pour en supprimer un, 3 pour supprimer toutes les occurences de ce nombre et 4 pour quitter"<<endl;
+		cin>>choice;
+
+		switch(choice){
+			case 1:
+				cout<<"Quel est votre nombre?"<<endl;
+				cin>>tmp_num;
+				num_list.push_back(tmp_num);
+				sort(num_list.begin(), num_list.end());
+				break;
+	
+			case 2:
+				cout<<"Quel nombre doit on supprimer?"<<endl;
+				cin>>tmp_num;
+				for (int i = 0; i < num_list.size(); ++i)
+				{
+					if (num_list[i] == tmp_num)
+					{
+						num_list.erase(num_list.begin()+i);
+						break;
+					}
+				}
+
+
+				break;
+
+			case 3:
+				cout<<"Quel nombre doit on supprimer?"<<endl;
+				cin>>tmp_num;
+				
+				for (int i = 0; i < num_list.size(); ++i)
+				{
+					if (num_list[i] == tmp_num)
+					{
+						num_list.erase(num_list.begin()+i);
+						i--;
+					}
+				}
+
+				break;
+			case 4:
+				cout<<"Au revoir"<<endl;
+				return 0;
 		}
 	}
 }
-
-int erase_given_num(float num, num_list, bool every_num){
-
-	for (int i = 0; i < num_list.size(); ++i)
-	{
-		if(every_num){
-			if(num_list[i] == num){
-				num_list.erase(num_list.begin()+i);
-			}
-			return 0;
-		}
-		else{
-			if(num_list[i] == num){
-				num_list.erase(num_list.begin()+i);
-			}
-		}
-	}
-	return 0;
-}
-
