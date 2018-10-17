958190a6f28b8b5ae1e6092c54984b69ac19a592
Guillaume.Gouhier <guillaume.gouhier1999@gmail.com>
2018-10-17 11:13:14 +0200

    add show sorted_list

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <string>
+#include <ctype.h>
+
+using namespace std;
+
+
 
 int main()
 {
-	float tmp_num;
-	std::cout<<"Quel est votre nombre?"<<std::endl;
-	std::cin>>tmp_num;
+	vector<int> num_list;
+	string tmp_input = "0";
 
-	std::cout<<tmp_num<<std::endl;
-	return 0;
+	float tmp_num;
+	while(true){
+		cout<<"Quel est votre nombre?"<<endl;
+		cin>>tmp_num;
+		num_list.push_back(tmp_num);
+		sort(num_list.begin(), num_list.end());
+		for(int i = 0; i < num_list.size(); i++){
+			cout<<num_list[i]<<endl;
+		}
+	}
 }
