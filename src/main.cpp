ae66c365cac99329f6782aac52a8dd710a4f6c8f
Guillaume.Gouhier <guillaume.gouhier1999@gmail.com>
2018-10-17 09:54:05 +0200

    add main.cpp & show_number

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	float tmp_num;
+	std::cout<<"Quel est votre nombre?"<<std::endl;
+	std::cin>>tmp_num;
+
+	std::cout<<tmp_num<<std::endl;
+	return 0;
+}
